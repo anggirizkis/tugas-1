39389e87dc6b3fb5b0bb0865734a48e82a94a3d6
anggirizkis <32116064+anggirizkis@users.noreply.github.com>
2017-09-22 10:44:53 +0700

    tugas alpro
    identitas

---
+++ tugas-1.py
@@ -0,0 +1,9 @@
+nama = "anggi rizki septiani"
+nim = "04217006"
+umur = 20
+print ("namaku : " + nama)
+print ("nimku : "+ nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan)
+print (umur +5 )
